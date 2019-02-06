ebcf4890957da43a400768921d1729ff9e73f20d
Jon Pelc <thisisjonpelc@gmail.com>
2019-02-06 17:38:52 -0600

    Completed large hero block and intro content block

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+
+const browserSync = require('browser-sync').create();
+
+gulp.task('watch', () => {
+
+    browserSync.init({
+        notify:false,
+        server:{
+            baseDir: 'app'
+        }
+    });
+
+    watch('./app/index.html', () => {
+        browserSync.reload();
+    });
+
+    watch('./app/assets/styles/**/**.css', () => {
+        gulp.start('cssInject');
+    });
+});
+
+gulp.task('cssInject', ['styles'], () => {
+
+    gulp.src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream());
+
+});
